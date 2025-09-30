1f1692b84fa61588f113c37e0544b84a25ab1da1
Joshua Wise <106165427+Joshua-Wise78@users.noreply.github.com>
2025-09-29 23:04:55 -0400

    9 database initalization   joshua (#13)
    * Database stuff man :)
    
    * Update database scripts
    
    * Name Update'
    
    * Modified scripts
    
    * Fixed names
    
    * Fixed formatting
    
    ---------
    
    Co-authored-by: Joshua Wise <joshua@Joshuas-MacBook-Air.local>

---
+++ Database-Design/Scripts/2_3_gas_station.sql
@@ -0,0 +1,11 @@
+USE karto;
+
+DROP TABLE IF EXISTS karto.gas_station
+
+CREATE TABLE gas_station (
+  station_id     INT UNSIGNED PRIMARY KEY NOT NULL
+  ,longitude     DECIMAL(8,6)             NOT NULL
+  ,latitude      DECIMAL(8,6)             NOT NULL
+  ,name          VARCHAR(31)              NOT NULL
+  ,address       VARCHAR(127)             NOT NULL
+);
---
+++ Database-Design/Scripts/3_2_gas_price.sql
@@ -0,0 +1,23 @@
+USE karto;
+
+DROP TABLE IF EXISTS karto.gas_price;
+
+CREATE TABLE gas_price 
+(
+   station_id      INT       UNSIGNED NOT NULL
+  ,gas_type_id     SMALLINT  UNSIGNED NOT NULL
+  ,price_usd_cents SMALLINT  UNSIGNED NOT NULL
+
+
+  ,PRIMARY KEY (station_id, gas_type_id)
+);
+
+ALTER TABLE gas_price 
+  ADD FOREIGN KEY (station_id) 
+  REFERENCES gas_station (station_id)
+;
+
+ALTER TABLE gas_price 
+  ADD FOREIGN KEY (gas_type_id) 
+  REFERENCES gas_type (gas_id)
+;
