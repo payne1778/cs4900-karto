ed8d72ce0d75c3fb4fbd56de975474511b597f29
JoshQuaintance <hasianjoshua@gmail.com>
2025-10-01 20:24:20 -0400

    Update scripts to reflect physical model

--- Database-Design/Scripts/1_2_gas_type.sql
+++ Database-Design/Scripts/1_2_gas_type.sql
@@ -7,15 +7,11 @@ DROP TABLE IF EXISTS karto.gas_type;
 CREATE TABLE gas_type
 (
     gas_id      SMALLINT UNSIGNED       NOT NULL        COMMENT 'PK (Natural) for Gas Type, Gas Ratings (e.g., 87, 89, 91, 93)',
-    gas_name    VARCHAR(31)             NOT NULL        COMMENT 'Gas Type Name',
+    gas_name    VARCHAR(15)             NOT NULL        COMMENT 'Gas Type Name',
 
     PRIMARY KEY (gas_id)
 )
 COMMENT 'Gas Type (Lookup Table)';
-
-ALTER TABLE gas_type
-    ADD CONSTRAINT gas_type_gas_id_uk01
-    UNIQUE (gas_id);
 
 -- Example Data
 INSERT INTO gas_type (gas_id, gas_name) VALUES
--- Database-Design/Scripts/2_2_user.sql
+++ Database-Design/Scripts/2_2_user.sql
@@ -5,12 +5,12 @@ DROP TABLE IF EXISTS karto.user;
 
 -- Create Table
 CREATE TABLE user (
-  email             VARCHAR(255)    NOT NULL,
+  user_email             VARCHAR(255)    NOT NULL,
   username          VARCHAR(31)     NOT NULL,
   hashed_password   VARCHAR(127)    NOT NULL,
   created_at        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  
-  PRIMARY KEY(email)
+
+  PRIMARY KEY(user_email)
 );
 
 ALTER TABLE user
