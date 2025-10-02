e96c9b0d8ae9d615329b1b55d2e47a863f009a5d
Owen Kemp <kemp.56@wright.edu>
2025-10-01 21:39:31 -0400

    Renamed gas_name to name

--- Database-Design/Scripts/1_2_gas_type.sql
+++ Database-Design/Scripts/1_2_gas_type.sql
@@ -7,14 +7,14 @@ DROP TABLE IF EXISTS karto.gas_type;
 CREATE TABLE gas_type
 (
     gas_type_id     SMALLINT UNSIGNED       NOT NULL        COMMENT 'PK (Natural) for Gas Type, Gas Ratings (e.g., 87, 89, 91, 93)',
-    gas_name        VARCHAR(15)             NOT NULL        COMMENT 'Gas Type Name',
+    name        VARCHAR(15)             NOT NULL        COMMENT 'Gas Type Name',
 
     PRIMARY KEY (gas_type_id)
 )
 COMMENT 'Gas Type (Lookup Table)';
 
 -- Example Data
-INSERT INTO gas_type (gas_type_id, gas_name) VALUES
+INSERT INTO gas_type (gas_type_id, name) VALUES
     (1, 'Regular'),
     (2, 'Mid-Grade'),
     (3, 'Premium'),
