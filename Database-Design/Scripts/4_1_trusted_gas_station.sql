12fadbaed5a620fcc9cceed9728022fb9e9e06a4
Porogaramu <112273919+Porogaramu@users.noreply.github.com>
2025-10-02 14:19:54 -0400

    Fixed Trusted Gas Stations Script (#30)

--- Database-Design/Scripts/4_1_trusted_gas_station.sql
+++ Database-Design/Scripts/4_1_trusted_gas_station.sql
@@ -6,18 +6,18 @@ DROP TABLE IF EXISTS karto.trusted_gas_station;
 -- Create Table
 CREATE TABLE trusted_gas_station (
   user_email    VARCHAR(255)  NOT NULL  COMMENT 'User Email',
-  gas_station   INT UNSIGNED  NOT NULL  COMMENT 'Gas Station',
+  station_id   INT UNSIGNED  NOT NULL  COMMENT 'Gas Station',
   
-  PRIMARY KEY (user, gas_station)
+  PRIMARY KEY (user_email, station_id)
 );
 
 -- References and Foreign Keys
 ALTER TABLE trusted_gas_station 
-    ADD FOREIGN KEY (user) 
+    ADD FOREIGN KEY (user_email) 
     REFERENCES user (user_email)
 ;
 
 ALTER TABLE trusted_gas_station 
-    ADD FOREIGN KEY (gas_station) 
+    ADD FOREIGN KEY (station_id) 
     REFERENCES gas_station (station_id)
 ;
