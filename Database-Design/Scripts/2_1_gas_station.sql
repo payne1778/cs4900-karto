6d5241762c3ed4892049ed7d52d30908ec4265e2
Joshua Wise <106165427+Joshua-Wise78@users.noreply.github.com>
2025-10-02 14:16:36 -0400

    3_1_gas_price updated (#26)
    * 3_1_gas_price updated
    
    * Added comments for gas_station & gas_price
    
    * Fixed primary key
    
    * Update using newest physical model
    
    ---------
    
    Co-authored-by: Joshua Wise <joshua@Joshuas-MacBook-Air.local>

--- Database-Design/Scripts/2_1_gas_station.sql
+++ Database-Design/Scripts/2_1_gas_station.sql
@@ -3,9 +3,16 @@ USE karto;
 DROP TABLE IF EXISTS karto.gas_station;
 
 CREATE TABLE gas_station (
-  station_id     INT UNSIGNED PRIMARY KEY NOT NULL
-  ,longitude     DECIMAL(8,6)             NOT NULL
-  ,latitude      DECIMAL(8,6)             NOT NULL
-  ,name          VARCHAR(31)              NOT NULL
-  ,address       VARCHAR(127)             NOT NULL
-);
+  station_id        INT UNSIGNED     NOT NULL    COMMENT 'Station Id',
+  longitude         DECIMAL(9,6)     NOT NULL    COMMENT 'Longitude',
+  latitude          DECIMAL(9,6)     NOT NULL    COMMENT 'Latitude',
+  name              VARCHAR(25)      NOT NULL    COMMENT 'Name of Gas Station',
+  address_line      VARCHAR(63)      NOT NULL    COMMENT 'Address of Gas Station',
+  city              VARCHAR(63)                  COMMENT 'City',
+  state             VARCHAR(31)                  COMMENT 'State',
+  zip_code          VARCHAR(10)                  COMMENT 'Zip Code',
+
+
+  PRIMARY KEY (station_id)
+)
+COMMENT 'Gas Station';
--- Database-Design/Scripts/3_1_gas_price.sql
+++ Database-Design/Scripts/3_1_gas_price.sql
@@ -4,13 +4,15 @@ DROP TABLE IF EXISTS karto.gas_price;
 
 CREATE TABLE gas_price 
 (
-   station_id      INT       UNSIGNED NOT NULL
-  ,gas_type_id     SMALLINT  UNSIGNED NOT NULL
-  ,price_usd_cents SMALLINT  UNSIGNED NOT NULL
+  station_id       INT UNSIGNED             NOT NULL    COMMENT 'Station Unique ID',
+  gas_type_id      SMALLINT UNSIGNED        NOT NULL    COMMENT 'Gas Type',
+  price_per_gallon DECIMAL(7, 4) UNSIGNED   NOT NULL    COMMENT 'Price per gallon of GAS',
+  last_updated     DATETIME                 NOT NULL DEFAULT CURRENT_TIMESTAMP
+  ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Updated Time',
 
-
-  ,PRIMARY KEY (station_id, gas_type_id)
-);
+  PRIMARY KEY (station_id, gas_type_id)
+)
+COMMENT 'Gas Price';
 
 ALTER TABLE gas_price 
   ADD FOREIGN KEY (station_id) 
